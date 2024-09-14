4e649c189ee127bafc6f62db2ffea1e3713b2fff
Saswat Dash <saswtadash098@gmail.com>
2024-09-14 12:44:48 +0530

    Added department routes and fixed server socket

--- server/routes/HospitalRoutes.ts
+++ server/routes/HospitalRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getHospitalIdByName, getHospitals } from "../controllers/HospitalController";
+import { getDepartments, getDoctors, getHospitalIdByName, getHospitals } from "../controllers/HospitalController";
 
 export const hospitalRoute = Router();
 
 hospitalRoute.get("/hospitalid", getHospitalIdByName);
 
 hospitalRoute.get("/hospitals", getHospitals);
+
+hospitalRoute.get("/deps/doctors", getDoctors);
+
+hospitalRoute.get("/departments", getDepartments);
